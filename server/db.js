44929757a0e3af27c97d8966adb035560547f1a8
sivaranjani <sivaaranjani.thangavel@gmail.com>
2022-03-20 10:49:14 -0700

    Initial setup for express and firebase

---
+++ server/db.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMMBvSW4eLG558j8KK35LSAHuHb5l6QDY",
+  authDomain: "fir-getting-started-bea8a.firebaseapp.com",
+  projectId: "fir-getting-started-bea8a",
+  storageBucket: "fir-getting-started-bea8a.appspot.com",
+  messagingSenderId: "1016376183699",
+  appId: "1:1016376183699:web:72076eb4081b1238c8b666"
+};
